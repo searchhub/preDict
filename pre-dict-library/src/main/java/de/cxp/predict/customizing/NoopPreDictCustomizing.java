547055b0daa96aa08d69d7837a00a5475908678d
Rudolf Batt <rudolf.batt@commerce-experts.com>
2017-06-13 07:57:36 +0200

    Add documentation and default impl to the customizing interface

--- pre-dict-library/src/main/java/de/cxp/predict/customizing/NoopPreDictCustomizing.java
+++ pre-dict-library/src/main/java/de/cxp/predict/customizing/NoopPreDictCustomizing.java
@@ -1,9 +1,6 @@
 package de.cxp.predict.customizing;
 
-import java.util.List;
-
 import de.cxp.predict.api.PreDictSettings;
-import de.cxp.predict.api.SuggestItem;
 import lombok.Getter;
 
 public class NoopPreDictCustomizing implements PreDictCustomizing {
@@ -17,37 +14,6 @@ public class NoopPreDictCustomizing implements PreDictCustomizing {
 	
 	public NoopPreDictCustomizing(PreDictSettings settings) {
 		this.settings = settings;
-	}
-
-	@Override
-	public String cleanIndexWord(String word) {
-		return word;
-	}
-
-	@Override
-	public String cleanSearchWord(String searchWord) {
-		return searchWord;
-	}
-
-	@Override
-	public double getReplacementDistance(char a, char b) {
-		return 1;
-	}
-	
-	@Override
-	public double adjustDistance(String searchWord, String candidate, double distance) {
-		return distance;
-	}
-
-	@Override
-	public double adjustDetailedDistance(String searchWord, String suggestion, double distance, int prefixLength,
-			int suffixLength) {
-		return distance;
-	}
-
-	@Override
-	public List<SuggestItem> adjustFinalResult(String searchWord, List<SuggestItem> result) {
-		return result;
 	}
 
 	@Override
--- pre-dict-library/src/main/java/de/cxp/predict/customizing/PreDictCustomizing.java
+++ pre-dict-library/src/main/java/de/cxp/predict/customizing/PreDictCustomizing.java
@@ -8,40 +8,84 @@ import de.cxp.predict.api.SuggestItem;
 
 public interface PreDictCustomizing {
 
-	String cleanIndexWord(String word);
+	/**
+	 * Modify word before it get's indexed.
+	 * Returns the unmodified word by default.
+	 * 
+	 * @param word
+	 * @return modified word
+	 */
+	default String cleanIndexWord(String word) {
+		return word;
+	};
 
-	String cleanSearchWord(String searchWord);
+	/**
+	 * Modify word before it is used to for search.
+	 * Returns the unmodified word by default.
+	 * 
+	 * @param searchWord the original search word
+	 * @return the modified search word
+	 */
+	default String cleanSearchWord(String searchWord) {
+		return searchWord;
+	};
 
 	PreDictSettings getSettings();
 
-	double getReplacementDistance(char a, char b);
-
-	double adjustDistance(String searchWord, String candidate, double distance);
+	/**
+	 * Returns the distance between two characters, which is 1 by default.
+	 * Can be used for a advanced keyboard distance calculation or similar.
+	 * 
+	 * @param char a
+	 * @param char b
+	 * @return
+	 */
+	default double getReplacementDistance(char a, char b) {
+		return 1.0;
+	};
 
 	/**
-	 * Adds the ability to adjust the distance calculated with the
-	 * edit distance logic (cxp+damerau+levenshtein), that has no access to the
-	 * common prefix and suffix of these words.
+	 * Adjust the final distance before adding the candidate to the result list.
+	 * Returns the unchanged given distance by default.
 	 * 
-	 * @param searchWord
+	 * @param searchWord the modified search word
+	 * @param candidate
+	 * @param distance
+	 * @return
+	 */
+	default double adjustDistance(String searchWord, String candidate, double distance) {
+		return distance;
+	};
+
+	/**
+	 * Adjust the distance after it got calculated with the edit distance logic
+	 * (cxp+damerau+levenshtein), that has no access to the common prefix and
+	 * suffix of these words.
+	 * Returns the unchanged given distance by default.
+	 * 
+	 * @param searchWord the modified search word
 	 * @param suggestion
 	 * @param distance
 	 * @param prefixLength
 	 * @param suffixLength
 	 * @return
 	 */
-	double adjustDetailedDistance(String searchWord, String suggestion, double distance, int prefixLength,
-			int suffixLength);
+	default double adjustDetailedDistance(String searchWord, String suggestion, double distance, int prefixLength,
+			int suffixLength) {
+		return distance;
+	};
 
 	/**
 	 * Can be used to filter and order the final result, which is already
 	 * truncated according to {@link AccuracyLevel} and sorted by
 	 * {@link SuggestItem.distance} and {@link SuggestItem.count}.
-	 * @param searchWord 
 	 * 
+	 * @param searchWord the modified search word
 	 * @param result
 	 * @return
 	 */
-	List<SuggestItem> adjustFinalResult(String searchWord, List<SuggestItem> result);
+	default List<SuggestItem> adjustFinalResult(String searchWord, List<SuggestItem> result) {
+		return result;
+	};
 
 }
